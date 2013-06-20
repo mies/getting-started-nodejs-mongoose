821fe44b6b012ccd6b4403f1f7d8e7da4047b123
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-06-20 13:32:51 +0200

    procfile

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.configure('test', function() {
 });
 
 app.configure('production', function() {
-  mongoose.connect('mongodb://' + process.env.process.env.MONGOLAB_URI + '/todos');
+  mongoose.connect('mongodb://' + process.env.MONGOLAB_URI + '/todos');
 });
 
 // Routes
